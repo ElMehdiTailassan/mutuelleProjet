994d21a62af3b1f3b3f51af54a70f0f65ed057e3
el mehdi <elmehdi.tailassan.ext@eulerhermes.com>
2023-12-01 21:53:32 +0100

    first commit

---
+++ src/main/java/com/example/medecinprojetmutuelle/MedecinProjetMutuelleApplication.java
@@ -0,0 +1,13 @@
+package com.example.medecinprojetmutuelle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedecinProjetMutuelleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedecinProjetMutuelleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/medecinprojetmutuelle/MedecinProjetMutuelleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medecinprojetmutuelle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedecinProjetMutuelleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
